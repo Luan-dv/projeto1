48afae1f1fd907a0d3d36717bb6bcd83d24a9b55
Laun S. Alves <luans.alves77@gmail.com>
2025-06-25 23:12:34 -0300

    Querys

--- receitas/models.py
+++ receitas/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+    
+    def __str__(self):
+        return self.name
+    
 
 class Recipe(models.Model):
     title =  models.CharField(max_length=65)
@@ -15,4 +22,11 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) #no momento da criação, ele gera uma data e não mexe depois!
     uptade_at = models.DateTimeField(auto_now=True) #somente esse campo atualiza
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d', blank=True, default='')
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
--- receitas/views.py
+++ receitas/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.receitas.factory import make_recipe
+from receitas.models import Recipe
+from django.http import Http404
 # from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'receitas/pages/home.html', context={
-        'receitas' : [make_recipe() for _ in range(10)]
+        'receitas' : recipes,
+        
+        # [make_recipe() for _ in range(10)]
 
     })
+
+def category(request, category_id):
+    # recipes = Recipe.objects.filter(category__id = category_id, is_published=True).order_by('-id')
+    #Estou pedindo para filtar os objetos por categoria e id por categoria
+
+    # category_name = getattr(
+    #     getattr(recipes.first(), 'category', None),
+    #     'name',
+    #     'Not Found'
+    # )
+    # if not recipes:
+    #     raise Http404('Not Found :/')
+    recipes = get_list_or_404(Recipe.objects.filter(category__id = category_id, is_published=True).order_by('-id'))
+    return render(request, 'receitas/pages/category.html', context={
+        'receitas' : recipes,
+        'title' : f'{recipes[0].category.name} - Category |'
+                    
+    })
+
+
 
 def recipe(request, id):
     return render(request, 'receitas/pages/recipe-view.html', context={
